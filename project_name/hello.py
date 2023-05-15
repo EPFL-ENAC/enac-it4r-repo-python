362669ee1d363f9c3ed2c9afb46dbc4721597cee
Régis Longchamp <97044425+regislon@users.noreply.github.com>
2023-05-15 13:43:41 +0200

    feat() : add sample python code

---
+++ project_name/hello.py
@@ -0,0 +1,12 @@
+import sys
+
+
+def get_python_version() -> str:
+    """Returns the Python version used to run this script.
+
+    Returns
+    -------
+    str
+        Python version
+    """
+    return sys.version
---
+++ tests/test_hello.py
@@ -0,0 +1,6 @@
+from project_name.hello import get_python_version
+
+
+def test_get_python_version():
+    # Test the return type
+    assert isinstance(get_python_version(), str)
